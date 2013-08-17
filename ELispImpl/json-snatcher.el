cfc504e3f519d7beb64f915eaa9cd2ad94e98097
Sterling Graham <sterlingrgraham@gmail.com>
2013-08-17 01:47:13 -0700

    Changed up some of the functions so they work properly

--- ELispImpl/json-snatcher.el
+++ ELispImpl/json-snatcher.el
@@ -9,16 +9,6 @@
 (setq jsons-tokens ())
 (setq jsons-num-regex "\\(-?\\(0\\|\\([1-9][[:digit:]]*\\)\\)\\(\\.[[:digit:]]+\\)?\\([eE][-+]?[[:digit:]]+\\)?\\)")
 
-
-(setq string_regex )
-
-"dogs";;"[^\"\\\\[:space:]]"
-(re-search-forward "[^\n\t\s]")
-(re-search-forward string_regex)
-
-
-
-"View from 15th Floor"
 (defun jsons-get-json-tokens (file)
   "Retrieves all of the strings in the current buffer."
   (with-temp-buffer
@@ -27,7 +17,7 @@
     (let* ((return_list ()) 
 	   (delim_regex "\\([\][\\{\\}:,]\\)")
 	   ;; TODO: Improve this regex. Although now it SEEMS to be working, and can be
-	   ;; used to validate escapes if needed later. The second half of it is pretty
+	   ;; used to validate escapes if needed later. The second half of the string regex it is pretty
 	   ;; pointless at the moment. I did it this way, so that the code closely mirrors
 	   ;; the RFC.
 	   (string_regex "\\(\"\\(\\([^\"\\\\\r\s\t\n]\\)*\\([\r\s\t\n]\\)*\\|\\(\\(\\\\\\\\\\)*\\\\\\(\\([^\r\s\t\n]\\|\\(u[0-9A-Fa-f]\\{4\\}\\)\\)\\)\\)\\)+\"\\)")
@@ -42,23 +32,26 @@
   "Advances the parser to the next token."
   (if jsons-tokens
       (prog1 
-	  (pop jsons-tokens)
+	  (message (pop jsons-tokens))
 	(setq jsons-curr-token (+ jsons-curr-token 1))
 	) 
     nil))
 
 (defun jsons-array ()
-  "Function called when a [ is encountered."
-  (let ((token (jsons-consume-token))
+  "Function called when a { is encountered."
+  (let*(
+	(token (jsons-consume-token))
 	(array (list "json-array" ())))
-    (while (not (string= token "]"))
-      (if (string= token ",")
-	  (setq token (jsons-consume-token))
-	(let ((json-value (jsons-value)))
-	  (push json-value (elt array 1))
-	  (setq token (jsons-consume-token))
-	  )))
-    array))
+    (progn
+      (while (not (string= token "]"))
+	(if (not (string= token ","))
+	    (let ((json-val (jsons-value token)))
+	      (push json-val (elt array 1))
+	      (setq token (jsons-consume-token))
+	      )
+	  (setq token (jsons-consume-token))))
+      array
+      )))
 
 (defun jsons-literal (token)
   "Returns a json-literal given by the current token. A literal is either true|false|null."
@@ -67,14 +60,14 @@
 
 (defun jsons-member (token)
   "Called when a member in a JSON object needs to be parsed."
-  (let ((member (list "json-member")))
-    (setq member (cons member token))
+  (let ((member ()))
+    (setq member (list "json-member" token))
     ;; TODO: Error checking... Should encounter a : here.
     (if  (not (string= (jsons-consume-token) ":"))
 	(error "Encountered token other than : in jsons-member.")
       nil
 	)
-    (setq member (cons member (cons (cdr member) (jsons-value))))
+    (setq member (list member (jsons-value (jsons-consume-token))))
     member))
 
 (defun jsons-number (token)
@@ -93,7 +86,7 @@ in the jsons-get-json-tokens function."
       (while (not (string= token "}"))
 	(if (not (string= token ","))
 	    (let ((json-mem (jsons-member token)))
-	      (puthash (elt json-mem 1) (elt json-mem 2) (elt object 1))
+	      (puthash (elt (elt json-mem 0) 1) (elt json-mem 1) (elt object 1))
 	      (setq token (jsons-consume-token))
 	      )
 	  (setq token (jsons-consume-token))))
@@ -101,27 +94,22 @@ in the jsons-get-json-tokens function."
       object
       )))
 
-(defun jsons-string ()
+(defun jsons-string (token)
   "Returns a json-string given by the current token."
-  (list "json-string" (elt jsons-curr-token jsons-tokens))
+  (list "json-string" token)
   )
-;;list "json-value" nil)
-(defun jsons-value ()
-  "A value, which is either an object, array, string, number, or literal."
 
+;;list "json-value" nil)
+(defun jsons-value (token)
+  "A value, which is either an object, array, string, number, or literal."
   (if (jsons-is-number token)
       (list "json-value" (jsons-number token))
-    
-    (let ((val (list "json-value"))
-	  (token (jsons-consume-token)))
-      (cond
-       ((string= token "{") (setq val (list val (jsons-object))))
-       ((string= token "[") (setq val (list val (jsons-array))))
-       ((string= token "\"") (setq val (list val (jsons-string token))))
-       (t (setq val (list val (jsons-literal token))))
-       )
-      val)
-    ))
+    (cond
+     ((string= token "{") (jsons-object))
+     ((string= token "[") (jsons-array))
+     ((string= (substring token 0 1) "\"") (jsons-string token))
+     (t (setq val (jsons-literal token))
+     ))))
 
  (defun string-integer-p (string)
    (if (string-match "\\`[-+]?[0-9]+\\'" string)
@@ -145,8 +133,7 @@ TODO: Fix this function to work properly."
      ((string= token "{") (jsons-object))
      ((string= token "[") (jsons-array))
      (t nil))
-    )
-)
+    ))
 
 (defun jsons-put-string (buffer str)
   "Append the text of the region to BUFFER."
@@ -154,15 +141,3 @@ TODO: Fix this function to work properly."
     (save-current-buffer
       (set-buffer (get-buffer-create buffer))
       (insert (prin1-to-string str))))
-
-(setq max-lisp-eval-depth 10000)
-
-(jsons-put-string "I can't believe it's not buffer!" (jsons-parse "~/Programming/Projects/json-snatcher/ELispImpl/Test/rfc_array.json"))
-
-(jsons-get-json-tokens "~/Programming/Projects/json-snatcher/ELispImpl/Test/aws.json")
-(setq max-mini-window-height 0.30)
-
-(setq jsons-tokens (jsons-get-json-tokens "~/Programming/Projects/json-snatcher/ELispImpl/Test/aws.json"))
-(jsons-consume-token)
-(setq debug-on-error t)
-
